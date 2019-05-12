5412c06b966ee6ccdf9199dff8ac643de0ae49ca
Anwesh43 <anweshthecool0@gmail.com>
2019-05-12 12:56:55 +0530

    adding CLRNode class

--- circlelineradiusview/src/main/java/com/anwesh/uiprojects/circlelineradiusview/CircleLineRadiusView.kt
+++ circlelineradiusview/src/main/java/com/anwesh/uiprojects/circlelineradiusview/CircleLineRadiusView.kt
@@ -128,4 +128,48 @@ class CircleLineRadiusView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CLRNode(var i : Int, val state : State = State()) {
+
+        private var next : CLRNode? = null
+        private var prev : CLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCLRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CLRNode {
+            var curr : CLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
