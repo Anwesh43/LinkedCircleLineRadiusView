0e5201ba432f7ba96a3d48a28a5aca43c4f06777
Anwesh43 <anweshthecool0@gmail.com>
2019-05-12 12:31:25 +0530

    adding drawCLRNode canvas extension method

--- circlelineradiusview/src/main/java/com/anwesh/uiprojects/circlelineradiusview/CircleLineRadiusView.kt
+++ circlelineradiusview/src/main/java/com/anwesh/uiprojects/circlelineradiusview/CircleLineRadiusView.kt
@@ -20,6 +20,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 3.4f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,34 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawCircleLineRadius(i : Int, size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    val sc1i : Float = sc1.divideScale(i, lines)
+    val sc2i : Float = sc2.divideScale(i, lines)
+    val r : Float = size / rFactor
+    save()
+    rotate(45f * (1f - 2 * i) * sc1i)
+    drawLine(0f, -size * sc2i, 0f, -size, paint)
+    paint.style = Paint.Style.FILL
+    drawCircle(0f, -size, r * sc2i, paint)
+    restore()
+}
+
+fun Canvas.drawCLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawCircle(0f, 0f, size, paint)
+    for (j in 0..(lines - 1)) {
+        drawCircleLineRadius(j, size, sc1, sc2, paint)
+    }
+    restore()
+}
