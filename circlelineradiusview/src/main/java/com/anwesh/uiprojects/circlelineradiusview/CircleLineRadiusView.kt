19ea5758dd566feb707e3bb2bba735cc25ab2b6f
Anwesh43 <anweshthecool0@gmail.com>
2019-05-12 12:59:23 +0530

    adding CircleLineRadius class

--- circlelineradiusview/src/main/java/com/anwesh/uiprojects/circlelineradiusview/CircleLineRadiusView.kt
+++ circlelineradiusview/src/main/java/com/anwesh/uiprojects/circlelineradiusview/CircleLineRadiusView.kt
@@ -169,7 +169,31 @@ class CircleLineRadiusView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class CircleLineRadius(var i : Int) {
+
+        private val root : CLRNode = CLRNode(0)
+        private var curr : CLRNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
